bfb273bfe615c2666305bceca18e1be3c28bf1b0
Sean Dougherty <dougherty@slu.edu>
2021-09-27 14:09:52 -0500

    finished animations for Experience/Timeline

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,5 +1,6 @@
 import { createUseStyles } from "react-jss";
 import { ReactComponent as Question } from "../resources/question.svg";
+import FadeIntoViewThin from "./Utility/FadeIntoViewThin";
 
 const Experience = (props) => {
   const classes = useStyles();
@@ -7,73 +8,107 @@ const Experience = (props) => {
     <section className={classes.page}>
       <span className={classes.bar} />
       {/* <span id="left-column" className={classes.left}> */}
-      <p
-        className={
-          classes.minor + " " + classes.leftEvent + " " + classes.event
-        }>
-        Entered Computer Science minor.
-      </p>
-      <p
-        className={
-          classes.phase + " " + classes.leftEvent + " " + classes.event
-        }>
-        Co-founded digital health startup surrounding remote cardiac
-        rehabilitation.
-      </p>
-      <p
-        className={
-          classes.research + " " + classes.leftEvent + " " + classes.event
-        }>
-        Entered Computer Security research lab with Dr. Reza Tourani (left
-        startup)
-      </p>
-      <p
-        className={
-          classes.mastersEnd + " " + classes.leftEvent + " " + classes.event
-        }>
-        Graduated Masters in C.S.
-      </p>
-      <p
-        className={
-          classes.question + " " + classes.leftEvent + " " + classes.event
-        }>
-        <Question />
-      </p>
+      <FadeIntoViewThin>
+        <p
+          className={
+            classes.minor + " " + classes.leftEvent + " " + classes.event
+          }>
+          Entered Computer Science minor.
+        </p>
+      </FadeIntoViewThin>
+      <FadeIntoViewThin>
+        <p
+          className={
+            classes.phase + " " + classes.leftEvent + " " + classes.event
+          }>
+          Co-founded digital health startup surrounding remote cardiac
+          rehabilitation.
+        </p>
+      </FadeIntoViewThin>
+      <FadeIntoViewThin>
+        <p
+          className={
+            classes.research + " " + classes.leftEvent + " " + classes.event
+          }>
+          Entered Computer Security research lab with Dr. Reza Tourani (left
+          startup)
+        </p>
+      </FadeIntoViewThin>
+      <FadeIntoViewThin>
+        <p
+          className={
+            classes.mastersEnd + " " + classes.leftEvent + " " + classes.event
+          }>
+          Graduated Masters in C.S.
+        </p>
+      </FadeIntoViewThin>
+      <FadeIntoViewThin>
+        <p
+          className={
+            classes.question + " " + classes.leftEvent + " " + classes.event
+          }>
+          <Question />
+        </p>
+      </FadeIntoViewThin>
       {/* </span> */}
       {/* <span id="middle-column" className={classes.timeline}> */}
-      <p className={classes.sixteen + " " + classes.year}>2016</p>
-      <p className={classes.seventeen + " " + classes.year}>2017</p>
-      <p className={classes.eighteen + " " + classes.year}>2018</p>
-      <p className={classes.nineteen + " " + classes.year}>2019</p>
-      <p className={classes.twenty + " " + classes.year}>2020</p>
-      <p className={classes.twentyone + " " + classes.year}>2021</p>
+      <FadeIntoViewThin>
+        <p className={classes.sixteen + " " + classes.year}>2016</p>
+      </FadeIntoViewThin>
+      <FadeIntoViewThin>
+        <p className={classes.seventeen + " " + classes.year}>2017</p>
+      </FadeIntoViewThin>
+      <FadeIntoViewThin>
+        <p className={classes.eighteen + " " + classes.year}>2018</p>
+      </FadeIntoViewThin>
+      <FadeIntoViewThin>
+        <p className={classes.nineteen + " " + classes.year}>2019</p>
+      </FadeIntoViewThin>
+      <FadeIntoViewThin>
+        <p className={classes.twenty + " " + classes.year}>2020</p>
+      </FadeIntoViewThin>
+      <FadeIntoViewThin>
+        <p className={classes.twentyone + " " + classes.year}>2021</p>
+      </FadeIntoViewThin>
       {/* </span> */}
       {/* <span id="right-column" className={classes.right}> */}
-      <p
-        className={
-          classes.undergrad + " " + classes.rightEvent + " " + classes.event
-        }>
-        Graduated B.S. in Biomedical engineering.
-      </p>
-      <p
-        className={
-          classes.mastersStart + " " + classes.rightEvent + " " + classes.event
-        }>
-        Entered Graduate program for Software Engineering.
-      </p>
-      <p
-        className={
-          classes.compSci + " " + classes.rightEvent + " " + classes.event
-        }>
-        Switched program focus from Software Engineering to Computer Science.
-      </p>
-      <p
-        className={
-          classes.thesis + " " + classes.rightEvent + " " + classes.event
-        }>
-        Thesis accepted to high-impact international security conference (ACM
-        CCS 2021).
-      </p>
+      <FadeIntoViewThin>
+        <p
+          className={
+            classes.undergrad + " " + classes.rightEvent + " " + classes.event
+          }>
+          Graduated B.S. in Biomedical engineering.
+        </p>
+      </FadeIntoViewThin>
+      <FadeIntoViewThin>
+        <p
+          className={
+            classes.mastersStart +
+            " " +
+            classes.rightEvent +
+            " " +
+            classes.event
+          }>
+          Entered Graduate program for Software Engineering.
+        </p>
+      </FadeIntoViewThin>
+      <FadeIntoViewThin>
+        <p
+          className={
+            classes.compSci + " " + classes.rightEvent + " " + classes.event
+          }>
+          Switched program focus from Software Engineering to Computer Science.
+        </p>
+      </FadeIntoViewThin>
+      <FadeIntoViewThin>
+        <p
+          className={
+            classes.thesis + " " + classes.rightEvent + " " + classes.event
+          }>
+          Thesis accepted to high-impact international security conference (ACM
+          CCS 2021).
+        </p>
+      </FadeIntoViewThin>
       {/* </span> */}
     </section>
   );
@@ -84,7 +119,6 @@ const useStyles = createUseStyles((theme) => ({
     backgroundColor: theme.colorPrimary,
     minHeight: "90vh",
     display: "grid",
-    // visibility: "hidden",
     gridTemplateColumns: [["10%", "1.5em", "80%"]],
     gridTemplateRows: [
       [
@@ -106,97 +140,7 @@ const useStyles = createUseStyles((theme) => ({
     gap: [[0, 0]],
     color: theme.colorSecondary,
     transition: [["background-color", "0.2s", "ease"]],
-    // position: "relative",
   },
-  // timeline: {
-  //   gridRow: [["1", "/", "14"]],
-  //   gridColumn: [["2", "/", "3"]],
-  //   display: "grid",
-  //   gridTemplateColumns: "1.5em",
-  //   gridTemplateRows: [
-  //     [
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //     ],
-  //   ],
-  //   alignItems: "center",
-  //   top: 0,
-  //   bottom: 0,
-  //   minHeight: "100%",
-  //   backgroundColor: theme.colorHighlight,
-  //   "& > p": {
-  //     color: theme.colorDarkest,
-  //     textOrientation: "upright",
-  //     writingMode: "vertical-rl",
-  //     fontWeight: "bold",
-  //     margin: [0, 0],
-  //   },
-  // },
-  // left: {
-  //   gridRow: [["1", "/", "14"]],
-  //   gridColumn: [["1", "/", "2"]],
-  //   display: "grid",
-  //   gridTemplateColumns: "10%",
-  //   gridTemplateRows: [
-  //     [
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //     ],
-  //   ],
-  //   alignItems: "center",
-  //   top: 0,
-  //   bottom: 0,
-  //   minHeight: "100%",
-  // },
-  // right: {
-  //   gridRow: [["1", "/", "14"]],
-  //   gridColumn: [["3", "/", "4"]],
-  //   display: "grid",
-  //   gridTemplateColumns: "80%",
-  //   gridTemplateRows: [
-  //     [
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //       "minmax(7em, auto)",
-  //       "minmax(5em, auto)",
-  //     ],
-  //   ],
-  //   alignItems: "center",
-  //   top: 0,
-  //   bottom: 0,
-  //   minHeight: "100%",
-  // },
   bar: {
     gridColumn: "2 / 3",
     gridRow: "1 / 14",
---
+++ src/components/Utility/FadeIntoViewThin.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { useState, useRef, useEffect } from "react";
+import { createUseStyles } from "react-jss";
+
+/* Similar to FadeIntoView, however instead of wrapping the
+ * child in a div that controls the animation. The FadeIn
+ * animation classes are directly given to the child component.
+ * Accepts only a SINGLE child.
+ */
+const FadeIntoViewThin = ({ children, ...props }) => {
+  const delay = props.delay ? props.delay + "ms" : "";
+  const [isVisible, setVisible] = useState(false);
+  const domRef = useRef();
+  const classes = useStyles(delay);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setVisible(true);
+        }
+      });
+    });
+    observer.observe(domRef.current);
+    return () => observer.unobserve(domRef.current);
+  }, []);
+
+  if (React.Children.only(children)) {
+    return React.cloneElement(children, {
+      className: `${children.props.className} ${classes.fadeIn} ${
+        isVisible ? "visible" : ""
+      }`,
+      ref: domRef,
+    });
+  } else {
+    return (
+      <div
+        className={`${classes.fadeIn} ${isVisible ? "visible" : ""}`}
+        ref={domRef}>
+        {props.children}
+      </div>
+    );
+  }
+};
+
+const useStyles = createUseStyles((theme) => ({
+  fadeIn: (delay) => ({
+    opacity: 0,
+    transform: "translateY(100%)",
+    visibility: "hidden",
+    transition: `opacity 0.6s ${delay} ease-out, transform 0.6s ${delay} ease-out`,
+    willChange: "opacity, visibility",
+    "&.visible": {
+      opacity: 1,
+      transform: "none",
+      visibility: "visible",
+    },
+  }),
+}));
+
+export default FadeIntoViewThin;
