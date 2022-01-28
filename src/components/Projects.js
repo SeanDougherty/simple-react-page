6615c8ac5682feed3a05da45eae569758cf8664b
Sean Dougherty <dougherty@slu.edu>
2022-01-28 13:18:50 -0600

    January 2021 update. Added APECS summary. Added new projects. Added new Banner Component for announcing news.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,9 @@
 import { createUseStyles } from "react-jss";
+import AdventOfCode21 from "./Projects/AdventOfCode21";
 import APECS from "./Projects/APECS";
 import CipherBreaker from "./Projects/CipherBreaker";
 import DES from "./Projects/DES";
+import ElectronMirror from "./Projects/ElectronMirror";
 import ProxySignature from "./Projects/ProxySignature";
 import UpwardsPlanarity from "./Projects/UpwardsPlanarity";
 import WebCrawler from "./Projects/WebCrawler";
@@ -13,6 +15,8 @@ const Projects = (props) => {
     <section className={classes.page}>
       <h2>Previous Projects</h2>
       <APECS />
+      <ElectronMirror />
+      <AdventOfCode21 />
       <ProxySignature />
       <WebCrawler />
       <CipherBreaker />
@@ -24,14 +28,14 @@ const Projects = (props) => {
 
 const useStyles = createUseStyles((theme) => ({
   page: {
-    backgroundColor: theme.colorSecondary,
+    backgroundColor: theme.colorPrimary,
     position: "relative",
     minHeight: "90vh",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
-    color: theme.colorPrimary,
+    color: theme.colorSecondary,
     transition: [["background-color", "0.2s", "ease"]],
     padding: "3rem 0",
     "& > h2": {
@@ -40,8 +44,8 @@ const useStyles = createUseStyles((theme) => ({
       left: "3%",
       margin: [["0.75rem", "0", "0.5rem", "1rem"]],
       padding: [[0, 9, 3, 0]],
-      borderBottom: [[theme.colorPrimary, "solid", 2]],
-      borderRight: [[theme.colorPrimary, "solid", 2]],
+      borderBottom: [[theme.colorSecondary, "solid", 2]],
+      borderRight: [[theme.colorSecondary, "solid", 2]],
     },
   },
   "@media only screen and (min-width: 768px)": {
