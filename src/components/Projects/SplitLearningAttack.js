18215cdab3a77137640195d6ce5619d2f1dba94c
Sean Dougherty <dougherty@slu.edu>
2022-02-02 12:16:15 -0600

    spelling correction

--- src/components/Projects/SplitLearningAttack.js
+++ src/components/Projects/SplitLearningAttack.js
@@ -5,7 +5,7 @@ import { ReactComponent as PYTORCH } from "../../resources/pytorch.svg";
 
 const title = "Split Learning - Attack & Defense [in submission]";
 const description =
-  "My final work as a graduate student. This paper explores the vulnerability of machine learning's most contemporary privacy-aware model design (i.e., split learning). In this work, we demonstrate an 'honest-but-curious' adversary's ability can infer private properties of clients' data without model poisoning or manipulation. In addition, we detail and analyze two solutions to this vulnerability.";
+  "My final work as a graduate student. This paper explores the vulnerability of machine learning's most contemporary privacy-aware DNN model design (i.e., split learning). In this work, we demonstrate an 'honest-but-curious' adversary's ability to infer private properties of clients' data without model poisoning or manipulation. In addition, we detail and analyze two solutions to this vulnerability.";
 
 const SplitLearningAttack = () => {
   return (
